cd2815feda4455a6a280bfb621ed758d9164b9d3
DiboraHaile <deborahaile3@gmail.com>
2023-10-12 14:56:34 +0300

    feat: add date and time parser

--- src/data_parser.py
+++ src/data_parser.py
@@ -1,4 +1,5 @@
 
+from datetime import datetime
 def prepare_for_record(track_id,value_list):
     record_list = []
     stripped = [*map(lambda x: x.strip(),value_list)]
@@ -18,3 +19,15 @@ def prepare_for_trajectory(value_list,location):
     trajectory_list.append(location)
     return track_id,trajectory_list
 
+def date_parser(date_string):
+    date_string = "20181024"
+    date_format = "%Y%m%d"
+    # Convert string to date
+    date_object = datetime.strptime(date_string, date_format).date()
+    return date_object
+
+def time_parser(time_string):
+    time_string = "0930"
+    time_format = "%H%M"
+    # Convert string to time
+    return datetime.strptime(time_string, time_format).time()
