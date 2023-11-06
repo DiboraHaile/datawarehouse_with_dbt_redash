30372d17c49d7ca5aa51380116caa0c2b97e8876
DiboraHaile <deborahaile3@gmail.com>
2023-11-06 13:51:16 +0300

    feat: check if a file is already ingested

--- airflow/dags/data_ingester.py
+++ airflow/dags/data_ingester.py
@@ -31,6 +31,10 @@ class IngestData:
         check_table_query = sql.SQL("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = %s)").format(sql.Identifier(table_name))
         self.cur.execute(check_table_query, (table_name,))
         self.table_exists = self.cur.fetchone()[0]
+        if self.table_exists:
+            print(f"Table trajectory already exists")
+        else:
+            print(f"Table trajectory doesn't exist")
 
 
     def initialize_db_connection(self):
@@ -53,6 +57,11 @@ class IngestData:
             print(parsed_info)
         return files,extracted
 
+    def already_ingested(self,file):
+        check_table_query = sql.SQL("SELECT EXISTS (SELECT filename FROM log WHERE filename = {})").format(sql.Literal(file))
+        self.cur.execute(check_table_query, (file,))
+        return self.cur.fetchone()[0]
+
     def ingest_to_db(self):
         trajectory_table = 'trajectory'
         record_table = 'record'
@@ -63,34 +72,59 @@ class IngestData:
         files,datetime_locations = self.extract_info_from_filename()
         print(f"Files {files}")
         for file,datetime_location in zip(files,datetime_locations):
-            df = pd.read_csv(file)
-            for i in range(df.shape[0]):
-                row = df.iloc[i,0].split(';')
-                track_id,trajectory_data = prepare_for_trajectory(row[0:4],datetime_location)
-                print(trajectory_data)
-                sql_trajectory = create_insert_stmt(trajectory_table,trajectory_cols)
-                self.cur.execute(sql_trajectory, trajectory_data)
-                for i in range(4,len(row),6):
-                    if len(row[i:i+6]) != 1:
-                        record_data = prepare_for_record(track_id,row[i:i+6])
-                        sql_record = create_insert_stmt(record_table,record_cols)
-                        print(record_data)
-                        self.cur.execute(sql_record, record_data)
-                        print("row inserted")
-                # new_id += 1
+            file_name = file.split('/')[-1].replace('.csv','')
+            if not self.already_ingested(file_name):
+                df = pd.read_csv(file)
+                for i in range(df.shape[0]):
+                    row = df.iloc[i,0].split(';')
+                    track_id,trajectory_data = prepare_for_trajectory(row[0:4],datetime_location)
+                    print(trajectory_data)
+                    sql_trajectory = create_insert_stmt(trajectory_table,trajectory_cols)
+                    self.cur.execute(sql_trajectory, trajectory_data)
+                    for i in range(4,len(row),6):
+                        if len(row[i:i+6]) != 1:
+                            record_data = prepare_for_record(track_id,row[i:i+6])
+                            sql_record = create_insert_stmt(record_table,record_cols)
+                            print(record_data)
+                            self.cur.execute(sql_record, record_data)
+                            print("row inserted")
+                print(f"{file_name} printing {type(file_name)}")
+                sql_log = create_insert_stmt('log',["filename"])
+                print(sql_log)
+                self.cur.execute(sql_log, (file_name,))
+                try:
+                    self.conn.commit()  
+                    self.initialize_db_connection()
+                except Exception as e:
+                    raise e
+                    # self.cur = self.conn.cursor() 
+                # except Exception as e:
+                #     print(f"Error: {e}")
+                #     self.conn.rollback()
+            else:
+                print("file already ingested")
+            
+            print(f"File {file_name} was ingested ")
         self.conn.commit()
+        self.cur.close()
         self.conn.close()
+
         print("ingestion successfull")
         
+    def drop_tables(self):
+        self.cur.execute("DROP TABLE IF EXISTS record;")
+        self.cur.execute("DROP TABLE IF EXISTS trajectory;")
+        print("tables trajectory and record successfully dropped")
+        self.conn.commit()
+        self.conn.close()
+
     def create_tables(self,replace=True):
         sql_create_trajectory = "create table trajectory (track_id int , vehicle_type varchar (50), traveled_d float8 check (traveled_d >= 0), avg_speed float8 check(avg_speed >= 0),geo_location int,recording_date date,time_min time,time_max time);"
-        # if replace:
-        #     self.cur.execute("DROP TABLE IF EXISTS record;")
-        #     self.cur.execute("DROP TABLE IF EXISTS trajectory;")
-        #     print("tables trajectory and record successfully dropped")
         self.cur.execute(sql_create_trajectory)
         sql_create_record = "create table record (track_id int,lat float8, lon float8, speed float8, lon_acc float8, lat_acc float8,record_time float8 check (record_time >= 0));"
         self.cur.execute(sql_create_record)
+        sql_create_log = "Create table log (filename varchar(60));"
+        self.cur.execute(sql_create_log)
         self.conn.commit()
         self.conn.close()
         print("tables trajectory and record successfully recreated")
@@ -139,3 +173,5 @@ if __name__ == '__main__':
     params = parser.parse_args()
     main(params)
      
+
+  
