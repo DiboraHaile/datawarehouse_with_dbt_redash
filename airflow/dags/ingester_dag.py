4c137725063d24596530f56feb04847e43584bc1
DiboraHaile <deborahaile3@gmail.com>
2023-11-09 21:15:20 +0300

    feat: add docker_operator for updating dbt models

--- airflow/dags/ingester_dag.py
+++ airflow/dags/ingester_dag.py
@@ -1,15 +1,19 @@
-from datetime import datetime, timedelta
-from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+from airflow import DAG
 from data_ingester import main
 import os
 from dotenv import load_dotenv
+from docker.types import Mount
+import logging
 
+logging.getLogger().setLevel(logging.DEBUG)
 
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 # Load environment variables from .env file
-load_dotenv()
+# load_dotenv()
 PG_HOST = os.getenv('POSTGRES_HOST')
 PG_USER = os.getenv('POSTGRES_USER')
 PG_PASSWORD = os.getenv('POSTGRES_PASSWORD')
@@ -18,22 +22,22 @@ PG_DATABASE = os.getenv('POSTGRES_DB')
 PATH = os.getenv('ABSOLUTE_PATH_PROJ')
 
 
-def ingester_callable(**kwargs):
-    op_kwargs=dict(
-                user=PG_USER,
-                password=PG_PASSWORD,
-                host=PG_HOST,
-                port=PG_PORT,
-                db=PG_DATABASE,
-                file_path=f"{PATH}/data/*.csv"
-            )
-    main(**op_kwargs)
+# def ingester_callable(**kwargs):
+#     op_kwargs=dict(
+#                 user=PG_USER,
+#                 password=PG_PASSWORD,
+#                 host=PG_HOST,
+#                 port=PG_PORT,
+#                 db=PG_DATABASE,
+#                 file_path=f"{PATH}/data/*.csv"
+#             )
+    # main(**op_kwargs)
 
 
 default_args = {
     'owner': 'dibora',
     'depends_on_past': False,
-    'start_date': datetime(2023, 11, 6),
+    'start_date': datetime(2023, 11, 8),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 0,
@@ -47,20 +51,50 @@ local_workflow = DAG(
     schedule_interval=timedelta(days=1),
 )
 
-print(f"hello {PG_HOST}")
+# print(f"hello {PG_HOST}")
 # Using BashOperator to execute a bash command
-with local_workflow:
-    get_env_task = BashOperator(
-            task_id="get_env",
-            bash_command="echo $POSTGRES_HOST ",
-        ),
-    ingest_task = PythonOperator(
-            task_id="ingest",
-            python_callable=ingester_callable,
+# with local_workflow:
+    # get_env_task = BashOperator(
+    #         task_id="get_env",
+    #         bash_command="echo $POSTGRES_HOST ",
+    #     ),
+    # ingest_task = PythonOperator(
+    #         task_id="ingest",
+    #         python_callable=ingester_callable,
 
+    #     ),
+run_dbt_task = DockerOperator(
+    task_id="update_dbt_model",
+    api_version='1.37',
+    docker_url='TCP://docker-socket-proxy:2375',
+    # command='dbt run',
+    image="dbt_image:v1",
+    network_mode="airflow_default",
+    tty=True,
+    dag=local_workflow,
+    mount_tmp_dir = False,
+    mounts=[
+        Mount(
+            target='/usr/app/', 
+            source=f'{PATH}/dbt_transformation/', 
+            type="bind"
+            ),
+        Mount(
+            target='/root/.dbt/',
+            source=f'{PATH}/dbt_transformation/profiles.yml',
+            type="bind"
         )
+        ],      
+        command="bash -c 'dbt build && dbt run'",
+        command="bash -c 'dbt docs generate'"
 
-get_env_task >> ingest_task
+    #     working_dir = '/app' dbt build && dbt docs generate
+        
+        )
+logging.info(f"DAG and DockerOperator configured successfully for {local_workflow.dag_id}")
+
+# get_env_task >> run_dbt_task
+# ingest_task >>
 # task1 = DockerOperator(
 #     task_id='task_1',
 #     image='ingester-ingester:latest',
